de08248149417f973b977c87e780564d58d3aacf
Glib Ischenko <glib.ischenko@emergn.com>
2022-05-02 11:11:35 +0300

    Prettify

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # remove users button app
+
+App to remove select users
+
+## Features:
+
+### 0.2.0
+
+* select all users
+
+### 0.1.0
+
+* select users one by one
+* delete selected users
--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,5 +1,5 @@
 export async function getUsers() {
-   const response = await fetch('https://gorest.co.in/public/v2/users');
-   const result = response.json();
-   return result;
+  const response = await fetch("https://gorest.co.in/public/v2/users");
+  const result = response.json();
+  return result;
 }
